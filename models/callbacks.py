4f97fb3251b040712d0f06f859bb2aa884113a84
Kiefer Macdonald <33000143+Kippus0s@users.noreply.github.com>
2026-03-12 12:17:31 +0000

    Update callbacks.py
    Correction

--- models/callbacks.py
+++ models/callbacks.py
@@ -100,4 +100,13 @@ class Plateau_decay_4(tf.keras.callbacks.ReduceLROnPlateau):
 )
 
 
-
+class Plateau_decay_5(tf.keras.callbacks.ReduceLROnPlateau):
+    lr_callback = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_accuracy',
+    factor=0.1,
+    patience=5,
+    verbose=0,
+    mode='auto',
+    min_delta=0.0001,
+    cooldown=0,
+    min_lr=1e-6,      
+)
