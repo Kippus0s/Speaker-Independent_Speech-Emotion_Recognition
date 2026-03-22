8fb0a0622d34d2be5a88f78853c95f2bfbd99f3c
Kiefer Macdonald <33000143+Kippus0s@users.noreply.github.com>
2026-03-22 20:01:54 +0000

    Update class_weights.py

--- utilities/class_weights.py
+++ utilities/class_weights.py
@@ -12,8 +12,7 @@ def class_weight_maker(ds):
     ds_unbatched = tuple(ds.unbatch()) #The datasets made are always batched and need to be unbatched to count the number of classes correctly
     TRAIN_DS_SIZE =  len(ds_unbatched)
     """Using method from "Class weights Calculate class weights" section in https://www.tensorflow.org/tutorials/structured_data/imbalanced_data """
-    #class_weight parameter in fit() requires a dictionary, so I iteratively fill it using the formula from above link, and the counts collection that was provided in the sample dataset
-    #Creating the class_weight dictionary
+    
     labels = []
     for (item,label) in ds_unbatched:
         labels.append(label.numpy())
@@ -21,11 +20,11 @@ def class_weight_maker(ds):
     counts = labels.value_counts().sort_index() # Counts variable contains count of all classes in the dataset    
     class_weight = {}
     x = 0
-    for i in counts.values: #Iterate through the values in counts        
+    for i in counts.values:   
         print("len counts num classes",len(counts)) 
-        y =  (1 / i) * TRAIN_DS_SIZE /len(counts) #Incase of discrepancy between this
+        y =  (1 / i) * TRAIN_DS_SIZE /len(counts) #In case of discrepancy between this
         print("y = ", y)
         class_weight.update({x: y}) 
-        x=x+1 #Incremement in loop variable to move on to setting count of the next class.
+        x=x+1 
     return class_weight
     
